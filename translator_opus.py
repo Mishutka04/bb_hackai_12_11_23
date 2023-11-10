c581c3d14b7312f947debdc984e71e7a8a354a84
KupchinskyDev <kupchinsky.dv@zerolab.tech>
2023-11-10 21:27:19 +0300

    add transletir to some langueges

---
+++ translator_opus.py
@@ -0,0 +1,36 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+def main(subtitles):
+    
+    with open(subtitles, 'r', encoding='utf-8') as file:
+        # Читаем содержимое файла
+        input_text = file.read()
+
+    # Загрузка токенизатора и модели для перевода с русского на ...
+    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+    model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+    # tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-fr")
+    # model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-fr")
+    # tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-ar")
+    # model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-ar")
+
+    # Пример текста на русском для перевода
+    # input_text = "Привет, как дела?"
+    
+    # Токенизация входного текста
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+    # Получение выходных токенов от модели
+    output_ids = model.generate(input_ids)
+
+    # Декодирование выходных токенов в текст
+    output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+    # Вывод результата
+    print("Исходный текст:", input_text)
+    print("Переведенный текст:", output_text)
+
+if __name__ == "__main__":
+    subtitles = "files/subtitles_0.srt"
+    main(subtitles=subtitles)
